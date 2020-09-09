6f01656b3fbb5a036a4e5000943d47dafba20a11
Dan <35927536+dan-mba@users.noreply.github.com>
2020-09-09 14:03:27 -0400

    limit topics to 30 & increase max font

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const getTopics = require("./lib/topics");
 const genSVG = require("./lib/word-cloud");
 
 (async function(){
-  const topicArr = await getTopics();
+  let topicArr = await getTopics();
   if (!topicArr || topicArr.length == 0) {
     console.log("Unable to get list of Topics from Github GraphQL API");
     throw new Error('GetTopicsErr');
   }
+
+  if (topicArr.length > 30) topicArr = topicArr.slice(0,30);
 
   const buffer = await genSVG(topicArr);
   try {
