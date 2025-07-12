976e5eb75d5ef36b776033b8bc9d7f4f3a5d2fe2
Dan <35927536+dan-mba@users.noreply.github.com>
2025-07-12 10:06:03 -0400

    add commented out code needed for test branch

--- index.ts
+++ index.ts
@@ -4,10 +4,15 @@ import getTopics from "./lib/topics.js";
 import genSVG from "./lib/word-cloud.js";
 
 (async function(){
-  const myToken = core.getInput('github-token');
+  // eslint-disable-next-line prefer-const
+  let myToken:string|undefined = core.getInput('github-token');
   if (!myToken) {
-    console.log('Required parameter "github-token" missing');
-    throw new Error('MissingParmErr');
+    //uncomment these lines to allow for test-branch action
+    //myToken = process.env.GITHUB_TOKEN;
+    //if (!myToken) {
+      console.log('Required parameter "github-token" missing');
+      throw new Error('MissingParmErr');
+    //}
   }
 
   const login = process.env.GITHUB_ACTOR;
