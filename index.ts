9dc768344c67a4117bd4a78af0ccd389c68d37f4
Dan <35927536+dan-mba@users.noreply.github.com>
2025-07-13 12:24:31 -0400

    switch to top level await

--- index.ts
+++ index.ts
@@ -3,35 +3,33 @@ import * as core from '@actions/core';
 import getTopics from "./lib/topics.js";
 import genSVG from "./lib/word-cloud.js";
 
-(async function(){
-  let myToken:string|undefined = core.getInput('github-token');
+let myToken:string|undefined = core.getInput('github-token');
+if (!myToken) {
+  myToken = process.env.GITHUB_TOKEN;
   if (!myToken) {
-    myToken = process.env.GITHUB_TOKEN;
-    if (!myToken) {
-      console.log('Required parameter "github-token" missing');
-      throw new Error('MissingParmErr');
-    }
+    console.log('Required parameter "github-token" missing');
+    throw new Error('MissingParmErr');
   }
+}
 
-  const login = process.env.GITHUB_ACTOR;
-  if (!login) {
-    console.log('Unable to access action actor');
-    throw new Error('MissingActorErr');
-  }
+const login = process.env.GITHUB_ACTOR;
+if (!login) {
+  console.log('Unable to access action actor');
+  throw new Error('MissingActorErr');
+}
 
-  let topicArr = await getTopics(login, myToken);
-  if (!topicArr || topicArr.length == 0) {
-    console.log("Unable to get list of Topics from Github GraphQL API");
-    throw new Error('GetTopicsErr');
-  }
+let topicArr = await getTopics(login, myToken);
+if (!topicArr || topicArr.length == 0) {
+  console.log("Unable to get list of Topics from Github GraphQL API");
+  throw new Error('GetTopicsErr');
+}
 
-  if (topicArr.length > 30) topicArr = topicArr.slice(0,30);
+if (topicArr.length > 30) topicArr = topicArr.slice(0,30);
 
-  const buffer = await genSVG(topicArr);
-  try {
-    fs.writeFileSync("cloud.svg", buffer);
-  } catch(e) {
-    console.log('Error writing "cloud.svg"');
-    throw e;
-  }
-})();
+const buffer = await genSVG(topicArr);
+try {
+  fs.writeFileSync("cloud.svg", buffer);
+} catch(e) {
+  console.log('Error writing "cloud.svg"');
+  throw e;
+}
